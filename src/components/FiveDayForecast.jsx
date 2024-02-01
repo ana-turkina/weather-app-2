ff9ae2b9fc4ee7851183ca2adb6bf22dbffb4c41
Anastassia <Anastassia.ana.turkina@gmail.com>
2024-02-01 21:04:12 +0200

    Simplify imports and update data in FiveDayForecast
    The FiveDayForecast component has been updated to simplify imports by bringing in only necessary Chart.js components. Default data and color schemes for the charts have been adjusted to more accurately reflect anticipated use case scenarios. A useState hook has also been included to prepare for real-time data injection.

--- src/components/FiveDayForecast.jsx
+++ src/components/FiveDayForecast.jsx
@@ -1,23 +1,7 @@
+import {useState} from "react";
+import {Chart as ChartJs} from 'chart.js/auto';
+import {Line} from 'react-chartjs-2';
 import './location-and-weather-details.css'
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip
-);
 
 export const options = {
     responsive: true,
@@ -30,32 +14,33 @@ export const options = {
         },
     },
 };
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = ['1', '2', '3', '4', '5'];
 export const data = {
     labels,
     datasets: [
         {
             label: 'Dataset 1',
-            data: 1,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            data: ['1', '2', '3'],
+            borderColor: '#FFC701',
+            backgroundColor: '#D9D9D9',
         },
         {
             label: 'Dataset 2',
-            data: 2,
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            data: ['3', '2', '1'],
+            borderColor: '#3569EE',
+            backgroundColor: '#D9D9D9',
         },
     ],
 };
 
 function FiveDayForecast() {
+    const [weatherData, setWeatherData] = useState(null);
     return (
         <>
             <div className="weather-details-column">
                 <div className="details-and-forecast-title">5-day Weather Forecast</div>
                 <div className="five-day-forecast-wrapper">
-                    <Line options={options} data={data} />
+                    <Line options={options} data={data}/>
                 </div>
             </div>
         </>
